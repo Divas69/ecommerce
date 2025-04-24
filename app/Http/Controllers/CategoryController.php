c824875f804141e819f61a861d59c625c8aa6157
Divas69 <divaschr@gmail.com>
2025-04-24 09:59:44 +0545

    Your commit message here

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,53 +4,54 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rules\Unique;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-       $categories = Category::orderBy('priority', 'asc')->get();
+        $categories = Category::orderBy('priority', 'asc')->get();
         return view('category.index',compact('categories'));
     }
-  public function create()
-  {
-    return view('category.create');
-  }
-public function store(Request $request)
-{
-   
-$data=$request->validate([
-    'name' => 'required',
-    'priority' => 'required|integer|gt:0|unique:categories,priority',
-],);
 
-Category::create($data);
-return redirect()->route('category.index');  
+    public function create()
+    {
+        return view('category.create');
+    }
 
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'priority' => 'required|integer|gt:0|unique:categories,priority',
+        ]);
 
-}
-public function edit($id)
-{
-    $category = Category::find($id);
-    return view('category.edit', compact('category'));
-}
-public function update(Request $request, $id)
-{
-    $data = $request->validate([
-        'name' => 'required',
-        'priority' => 'required|integer|gt:0|unique:categories,priority,' .$id,
-    ]);
+        Category::create($data);
+        return redirect()->route('category.index');
+    }
 
-    $category = Category::find($id);
-    $category->update($data);
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        return view('category.edit',compact('category'));
+    }
 
-    return redirect()->route('category.index');
-}
-public function destroy(request $request)
-{
-    $category = Category::find($request->id);
-    $category->delete();
-    return redirect()->route('category.index');
-}
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'priority' => 'required|integer|gt:0|unique:categories,priority,'.$id,
+        ]);
+
+        $category = Category::find($id);
+        $category->update($data);
+        return redirect()->route('category.index');
+    }
+
+    public function destroy(Request $request)
+    {
+        $category = Category::find($request->id);
+        $category->delete();
+        return redirect()->route('category.index');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,30 +10,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'dashboard'])
-->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class,'dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-  
+Route::middleware('auth')->group(function(){
 
+    //Category Routes
+    Route::get('/category',[CategoryController::class, 'index'])->name('category.index');
+    Route::get('/category/create',[CategoryController::class, 'create'])->name('category.create');
+    Route::post('/category/store',[CategoryController::class, 'store'])->name('category.store');
+    Route::get('/category/{id}/edit',[CategoryController::class, 'edit'])->name('category.edit');
+    Route::post('/category/{id}/update',[CategoryController::class, 'update'])->name('category.update');
+    Route::post('/category/destroy',[CategoryController::class,'destroy'])->name('category.destroy');
 
-
-//category Routes
-route::get('/category',[CategoryController::class,'index'])->name('category.index');
-
-Route::get('/category/create',[CategoryController::class,'create'])->name('category.create');
-Route::post('/category/store',[CategoryController::class,'store'])->name('category.store');
-Route::get('/category/{id}/edit',[CategoryController::class,'edit'])->name('category.edit');
-Route::post('/category/{id}/update',[CategoryController::class,'update'])->name('category.update');
-Route::post('/category/destroy',[CategoryController::class,'destroy'])->name('category.destroy');
+    //Product Routes
+    Route::get('/products',[ProductController::class, 'index'])->name('product.index');
 
 });
-
-
-
-
-
-Route::middleware('auth')->group(function () {
+    Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
