3fa26b3ea09cdaa0d427912193ccee844b661af2
Divas69 <divaschr@gmail.com>
2025-04-30 08:53:07 +0545

    product edit and delete update

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -15,22 +15,49 @@
             <th class="p-3 border border-gray-300">Category</th>
             <th class="p-3 border border-gray-300">Action</th>
         </tr>
-        @foreach ($products as $product)
-            <tr class="text-center">
+        @foreach($products as $product)
+        <tr class="text-center">
             <td class="p-3 border">
-                <div class="flex justify-center">
-                    <img src="{{ asset('images/' . $product->photopath) }}" 
-                    alt="" class="h-30 w-20">
-                </div>
-               </td>
+                <img src="{{asset('images/'.$product->photopath)}}" alt="" class="h-16">
+            </td>
             <td class="p-3 border">{{$product->name}}</td>
             <td class="p-3 border">{{$product->description}}</td>
             <td class="p-3 border">{{$product->price}}</td>
             <td class="p-3 border">{{$product->discounted_price}}</td>
             <td class="p-3 border">{{$product->stock}}</td>
             <td class="p-3 border">{{$product->category->name}}</td>
-            <td class="p-3 border">Edit Delete</td>
+            <td class="p-3 border">
+                <a href="{{route('product.edit', $product->id)}}" class="bg-blue-500 text-white px-4 py-2 rounded-lg">Edit</a>
+                <a onclick="deleteFunction('{{$product->id}}')" class="bg-red-600 text-white px-4 py-2 rounded-lg cursor-pointer">Delete</a>
+            </td>
         </tr>
         @endforeach
     </table>
+
+    <div id="delete-popup" class="hidden fixed inset-0 bg-blue-600 bg-opacity-60 backdrop-blur-sm  items-center justify-center">
+        <form action="{{route('product.destroy')}}" method="POST" class="bg-white px-10 py-6 rounded-lg shadow text-center">
+            @csrf
+            <input type="hidden" name="id" id="dataid">
+            <p class="font-bold text-lg">Are You Sure to Delete ?</p>
+            <div class="flex gap-4 mt-5">
+                <button type="submit" class="bg-blue-600 text-white px-10 py-2 rounded-lg">Yes</button>
+                <div onclick="hidePopup()" class="bg-red-600 text-white px-10 py-2 rounded-lg cursor-pointer">No</div>
+            </div>
+        </form>
+    </div>
+
+    <script>
+        function deleteFunction(id)
+        {
+            document.getElementById('delete-popup').classList.remove('hidden');
+            document.getElementById('delete-popup').classList.add('flex');
+            document.getElementById('dataid').value = id;
+        }
+
+        function hidePopup()
+        {
+            document.getElementById('delete-popup').classList.remove('flex');
+            document.getElementById('delete-popup').classList.add('hidden');
+        }
+    </script>
 @endsection
